a80befc98e01e7a462384fa6c8214a8ce09f2776
Richard Bennett <inbox@richardbennett.dev>
2022-11-05 22:32:00 -0400

    Adds a little logging to understand when the clock is actually being called.

--- CircuitBreakerPattern/Clock/Controllers/TimeIsController.cs
+++ CircuitBreakerPattern/Clock/Controllers/TimeIsController.cs
@@ -3,7 +3,16 @@ namespace Clock.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 public class TimeIsController : Controller {
-    public IActionResult Index() => Switches.ApiShouldThrowException
-        ? BadRequest()
-        : Json(new { Time = DateTime.UtcNow });
+    private readonly ILogger _logger;
+
+    public TimeIsController(ILoggerFactory loggerFactory) {
+        _logger = loggerFactory.CreateLogger<TimeIsController>();
+    }
+
+    public IActionResult Index() {
+        _logger.LogInformation($"Clock is being queried on {DateTime.UtcNow} and will {(Switches.ApiShouldThrowException ? "NOT " : "")}return the current time.");
+        return Switches.ApiShouldThrowException
+            ? BadRequest()
+            : Json(new { Time = DateTime.UtcNow });
+    }
 }
